244f2a8b38ab6b5a0072047084139a7e16d73d31
Laabidi RAISSI <laabidi.raissi@gmail.com>
2015-06-20 17:20:48 +0200

    Solution to the Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The method creates a special matrix (pretty much like the special vector in the example)
+## the internal inverse attribute will store the inverse of the matrix. Based on its value
+## we decide wether the inverse has been calculated or not.
+## When data of the special matrix is changed (only possible via set function) the inverse will be set to NULL
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+  inverse <- NULL
+  set <- function(y) {
+    m <<- y
+    inverse <<- NULL
+  }
+  get <- function() m
+  
+  setinverse <- function(inv) inverse <<- inv
+  getinverse <- function() inverse
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## given a special matrix created by makeCacheMatrix this method 
+## checks if its inverse is already calculated and so returns it
+##  otherwise, it calculates it and stores it in the special matrix for futur use
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m, ...){
+  
+  res <- m$getinverse()
+  if(!is.null(res)) {
+    message("getting cached data")
+    return(res)
+  }
+  #Either this is the first time 
+  #to calculate the inverse or the content of m was modified
+  message("Need to calculate the inverse")
+  data <- m$get()
+  res <- solve(data, ...)
+  m$setinverse(res)
+  res
 }
